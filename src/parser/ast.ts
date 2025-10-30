93c0eaf7145b9948de51e1a0a667453de81c0ff1
Gizmal <vincent.denavit@gmail.com>
2025-10-30 16:48:16 +0100

    Add AST interfaces for RPG document structure

---
+++ src/parser/ast.ts
@@ -0,0 +1,45 @@
+export interface Position {
+  line: number;
+  character: number;
+}
+
+export interface Range {
+  start: Position;
+  end: Position;
+}
+
+export interface BaseNode {
+  name?: string;
+  range: Range;
+}
+
+export interface Procedure extends BaseNode {
+  kind: 'procedure';
+  name: string;
+}
+
+export interface Variable extends BaseNode {
+  kind: 'variable';
+  name: string;
+  dclType: string;
+}
+
+export interface DataStructure extends BaseNode {
+  kind: 'dataStructure';
+  name: string;
+}
+
+export interface ToDo extends BaseNode {
+  kind: 'toDo';
+  text: string;
+}
+
+export type RpgSymbol = Procedure | Variable | DataStructure | Todo;
+
+export interface RpgDocument {
+  symbols: RpgSymbol[];
+  metrics: {
+    controlBlocks: number;
+    todos: number;
+  };
+}
