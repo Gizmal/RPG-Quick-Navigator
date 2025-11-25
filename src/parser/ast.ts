636d416af227fba6aa34e34348b683cc440971f9
Gizmal <vincent.denavit@gmail.com>
2025-11-25 10:12:24 +0100

    Add new interfaces and update RpgSymbol type

--- src/parser/ast.ts
+++ src/parser/ast.ts
@@ -15,17 +15,63 @@ export interface BaseNode {
 export interface Procedure extends BaseNode {
   kind: 'procedure';
   name: string;
+  isExport: boolean;
+}
+
+export interface Subroutine extends BaseNode {
+  kind: 'subroutine';
+  name: string;
+}
+
+export interface Constant extends BaseNode {
+  kind: 'constant';
+  name: string;
+  value: string;
 }
 
 export interface Variable extends BaseNode {
   kind: 'variable';
   name: string;
   dclType: string;
+  isTab: boolean;
+  tabDim?: string;
 }
 
 export interface DataStructure extends BaseNode {
   kind: 'dataStructure';
   name: string;
+  options?: string;
+  values: ItemDS[];
+  isTab: boolean;
+  tabDim?: string;
+}
+
+export interface ItemDS extends BaseNode {
+  kind: 'itemDS';
+  name: string;
+  dclType: string;
+  isTab?: boolean;
+  tabDim?: string;
+}
+
+export interface Enum extends BaseNode {
+  kind: 'enum';
+  name: string;
+  options?: string;
+  values: ItemEnum[];
+}
+
+export interface ItemEnum extends BaseNode {
+  kind: 'itemEnum';
+  name: string;
+  value: string;
+}
+
+export interface DeclaredFile extends BaseNode {
+  kind: 'declaredFile';
+  name: string;
+  fileType: string;
+  fileOptions: string;
 }
 
 export interface ToDo extends BaseNode {
@@ -33,7 +79,17 @@ export interface ToDo extends BaseNode {
   text: string;
 }
 
-export type RpgSymbol = Procedure | Variable | DataStructure | ToDo;
+export type RpgSymbol = 
+  Procedure     | 
+  Subroutine    | 
+  Constant      | 
+  Variable      | 
+  DataStructure | 
+  ItemDS        | 
+  Enum          | 
+  ItemEnum      | 
+  DeclaredFile  | 
+  ToDo;
 
 export interface RpgDocument {
   symbols: RpgSymbol[];
